ee1509cfd1f21a4a552bfdc0c19314ef85e466ba
Tridet <theo.lacour@ecl15.ec-lyon.fr>
2017-06-28 12:13:05 +0900

    Slight changes

--- dense_features.py
+++ dense_features.py
@@ -22,18 +22,18 @@ print('TensorFlow version: {0}'.format(tf.__version__))
 t = time.time()
 
 X_train1 = ET.parse(
-    r"C:\Users\Theo\Desktop\jMIR_3_0_developer\jMIR_3_0_developer\jSymbolic2\dist\extracted_feature_values_train_jazz.xml").getroot()
+    r"jSymbolic2\features\extracted_feature_values_train_jazz.xml").getroot()
 X_train2 = ET.parse(
-    r"C:\Users\Theo\Desktop\jMIR_3_0_developer\jMIR_3_0_developer\jSymbolic2\dist\extracted_feature_values_train_rap.xml").getroot()
+    r"jSymbolic2\features\extracted_feature_values_train_rap.xml").getroot()
 X_test1 = ET.parse(
-    r"C:\Users\Theo\Desktop\jMIR_3_0_developer\jMIR_3_0_developer\jSymbolic2\dist\extracted_feature_values_test_jazz.xml").getroot()
+    r"jSymbolic2\features\extracted_feature_values_test_jazz.xml").getroot()
 X_test2 = ET.parse(
-    r"C:\Users\Theo\Desktop\jMIR_3_0_developer\jMIR_3_0_developer\jSymbolic2\dist\extracted_feature_values_test_rap.xml").getroot()
+    r"jSymbolic2\features\extracted_feature_values_test_rap.xml").getroot()
 
 print("XML files parsed")
 
 
-def toArray(X, y):
+def parse(X, y):
     out = []
     for song in X[1:]:  # remove the first
         features = []
@@ -43,90 +43,103 @@ def toArray(X, y):
     return out, [y for k in range(len(X[1:]))]
 
 
-xt1, yt1 = toArray(X_train1, 1)
-xt2, yt2 = toArray(X_train2, 0)
-X_train = np.array(xt1 + xt2)
-y_train = np.array(yt1 + yt2)
+def data_process(X_train1, X_train2, X_test1, X_test2):
+    xt1, yt1 = parse(X_train1, 1)
+    xt2, yt2 = parse(X_train2, 0)
+    X_train = np.array(xt1 + xt2)
+    y_train = np.array(yt1 + yt2)
 
-xt1, yt1 = toArray(X_test1, 1)
-xt2, yt2 = toArray(X_test2, 0)
-X_test = np.array(xt1 + xt2)
-y_test = np.array(yt1 + yt2)
+    xt1, yt1 = parse(X_test1, 1)
+    xt2, yt2 = parse(X_test2, 0)
+    X_test = np.array(xt1 + xt2)
+    y_test = np.array(yt1 + yt2)
+
+    X_tot = np.concatenate((X_test, X_train))
+    y_tot = np.concatenate((y_test, y_train))
+
+    # Standardization
+    def standardize(X):
+        scaler = StandardScaler().fit(X)
+        rescaled = scaler.transform(X)
+        return rescaled
+
+    X_tot = standardize(X_tot)
+
+    # Randomization
+    inds = np.random.permutation(X_tot.shape[0])
+    train_inds, test_inds = inds[:int(0.8 * len(inds))], inds[int(0.8 * len(inds)):]  # 80% training set, 20% testing set
+    X_train = X_tot[train_inds]
+    X_test = X_tot[test_inds]
+    y_train = y_tot[train_inds]
+    y_test = y_tot[test_inds]
+
+    return X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train, y_test = data_process(X_train1, X_train2, X_test1, X_test2)
+
 
 # GBM
-clf = GradientBoostingClassifier()
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
+def call_GMB():
+    clf = GradientBoostingClassifier()
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
 
-print("GBM accuracy = ", accuracy, "Precision = ", precision_score(y_test, y_pred))
+    print("GBM accuracy = ", accuracy, "Precision = ", precision_score(y_test, y_pred))
 
-print("X_train.shape : {0}\nX_test.shape : {1}".format(X_train.shape, X_test.shape))
-print("y_train.shape : {0}\ny_test.shape : {1}".format(y_train.shape, y_test.shape))
+    print("X_train.shape : {0}\nX_test.shape : {1}".format(X_train.shape, X_test.shape))
+    print("y_train.shape : {0}\ny_test.shape : {1}".format(y_train.shape, y_test.shape))
 
-
-def standardize(X):
-    scaler = StandardScaler().fit(X)
-    rescaled = scaler.transform(X)
-    return rescaled
-
-
-X_train = standardize(X_train)
-X_test = standardize(X_test)
+#call_GMB()
 
 # learning parameters
 learning_rate = 0.001
 epoches = 10000
 batch_size = 100
-dropout = 0.6
+dropout = 0.5
 dense_layers = 600
 
-'''# 1st model : LSTM
-model = Sequential([
-    LSTM(128, return_sequences=True,input_shape=(156, 1,)),
-    Dropout(0.25),
-    LSTM(128,return_sequences=False),
-    Dropout(0.25),
-    Dense(128,activation="relu"),
-    Dense(1, activation="sigmoid")
-])'''
 
-# 2nd model : Dense layers
-model = Sequential([
-    Dense(dense_layers, activation="relu", input_shape=(156,)),
-    Dropout(dropout),
-    Dense(dense_layers, activation="relu"),
-    Dropout(dropout),
-    Dense(dense_layers, activation="relu"),
-    Dropout(dropout),
-    Dense(300, activation="relu"),
-    Dense(1, activation="sigmoid")
-])
+def run_model():
+    # 1st model : Dense layers
+    model = Sequential([
+        Dense(dense_layers, activation="relu", input_shape=(156,)),
+        Dropout(dropout),
+        Dense(dense_layers, activation="relu"),
+        Dropout(dropout),
+        Dense(dense_layers, activation="relu"),
+        Dropout(dropout),
+        Dense(300, activation="relu"),
+        Dropout(dropout),
+        Dense(1, activation="sigmoid")
+    ])
 
-# Custom optimizer
-sgd = keras.optimizers.SGD(lr=learning_rate)
+    # Custom optimizer
+    sgd = keras.optimizers.SGD(lr=learning_rate)
 
-# Choice of optimizer, loss and metrics
-model.compile(optimizer=sgd,
-              loss='binary_crossentropy',
-              metrics=['binary_accuracy'])
+    # Choice of optimizer, loss and metrics
+    model.compile(optimizer=sgd,
+                  loss='binary_crossentropy',
+                  metrics=['binary_accuracy'])
 
-# Training
-hist1 = model.fit(X_train, y_train, epochs=int(epoches * 2 / 3), batch_size=batch_size, verbose=2)
-model.optimizer.lr = tf.constant(0.0001)
-hist2 = model.fit(X_train, y_train, epochs=int(epoches / 3), batch_size=batch_size, verbose=2)
+    # Training
+    hist1 = model.fit(X_train, y_train, epochs=int(epoches * 2 / 3), batch_size=batch_size, verbose=2)
+    model.optimizer.lr = tf.constant(learning_rate / 10)
+    hist2 = model.fit(X_train, y_train, epochs=int(epoches / 3), batch_size=batch_size, verbose=2)
 
-# Testing
-score = model.evaluate(X_test, y_test, batch_size=32)
+    # Testing
+    score = model.evaluate(X_test, y_test)
 
-print("\nScore :", score)
-print("\nDuration :", time.time() - t)
+    print("\nScore :", score)
+    print("\nDuration :", time.time() - t)
 
-# Saving the model
-model.save("dense_xml_jazz_rap.h5")
+    # Saving the model
+    model.save(r"models/dense_xml_jazz_rap_%.3f.h5" % score[1])
 
-plt.plot(hist1.history.get("binary_accuracy") + hist2.history.get("binary_accuracy"))
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.axis([0, epoches, 0, 1])
-plt.savefig(r'plot/Dense_4layers%s_%.1fdropout_%sbatch_%.3ftest.png' % (dense_layers, dropout, batch_size, score[1]))
+    plt.plot(hist1.history.get("binary_accuracy") + hist2.history.get("binary_accuracy"))
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.axis([0, epoches, 0, 1])
+    plt.savefig(r'plot/Dense_4layers%s_%.1fdropout_%sbatch_%.3ftest.png' % (dense_layers, dropout, batch_size, score[1]))
+
+run_model()
--- jSymbolic.py
+++ jSymbolic.py
@@ -1,17 +1,26 @@
 import subprocess
 import os
+import xml.etree.ElementTree as ET
+
 
 def get_features(midi):
     """function creates feature_values.xml file which contains features for subsequent parsing """
     # after calling this function jSymbolic will create 3 useless (in our case) files
-    subprocess.call(['java', '-Xmx3048m', '-jar', 'jSymbolic2/jSymbolic2.jar', midi,
+    subprocess.call(['java', '-Xmx3048m', '-jar', 'jSymbolic2/dist/jSymbolic2.jar', midi,
                      'feature_values.xml', 'feature_descriptions.xml'])
 
-    files = ['feature_descriptions.xml', 'feature_values.csv', 'feature_values.arff']
-    for file in files: os.remove(file)
+    # jSymbolic can create csv of arff files if some special features are extracted, it is not the case here
+    files = ['feature_descriptions.xml']
+    os.remove(files[0])
 
-    # TODO: parse the features from feature_values.xml here
+    X = ET.parse('feature_values.xml').getroot()
+
+    features = []
+    for song in X[1:]:  # remove the header
+        for feature in song[1:]:  # remove the header
+            features.append(float(feature[1].text.replace(',', '.')))  # commas in XML files have to be turned into dot
 
     os.remove('feature_values.xml')
 
-    # TODO: return parsed object
+    # since there is only one song to extract, the output is just a list of the values of the features
+    return features
